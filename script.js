153ba67fd937ea55b5040f1332a6e4e5e77d72fc
du-rock <111354751+du-rock@users.noreply.github.com>
2024-05-27 09:04:43 -0300

    fg

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = ["pergunta1" "pergunta2"]
+const perguntas = [
+    {
+    enunciado: "pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Aternativa 2"
+        ]
+}
+{
+    enunciado: "pergunta 2", 
+    alternativas: [
+        "Alternativa 1",
+        "Aternativa 2"
+        ]
+}
+]
